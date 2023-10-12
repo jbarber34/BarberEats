e8f584549362526ad2569dd1cb84c959f275d106
James Barber <jpbarber34@gmail.com>
2023-10-12 13:24:33 -0400

    Create the Recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes = [];
 
+  constructor() {}
+
+  ngOnInit() {}
 }
