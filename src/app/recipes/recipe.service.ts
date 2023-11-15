44433fe7a5742e1de968e1176fa7e1e0d15768b8
James Barber <jpbarber34@gmail.com>
2023-11-15 08:53:35 -0500

    Transforming Response Data; Remove default recipes + ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,32 +8,34 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Teriyaki Chicken',
-      'Chicken breast chunks with a teriyaki sauce served with rice and broccoli.',
-      'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2F01410577b65b0fa720ffaf8a514172d4e4c82e4f',
-      [
-        new Ingredient('Chicken Breast', 6, 'oz'),
-        new Ingredient('White Rice', 2, 'cup(s)'),
-        new Ingredient('Broccoli', 2, 'cup(s)'),
-      ]
-    ),
-    new Recipe(
-      'Cowboy Burger',
-      'Double cheeseburger with bacon, an onion ring, jalapenos, and BBQ sauce.',
-      'https://iamhomesteader.com/wp-content/uploads/2023/05/rodeo-burger-800x736.jpg',
-      [
-        new Ingredient('80/20 Ground Beef', 1, 'lb(s)'),
-        new Ingredient('Hamburger Buns', 6, 'each'),
-        new Ingredient('Pepperjack Cheese', 6, 'slices'),
-        new Ingredient('Bacon', 1, 'lb'),
-        new Ingredient('Onion Rings', 6, 'each'),
-        new Ingredient('Jalapeno', 2, 'each'),
-        new Ingredient('BBQ sauce of choice', 1, 'each'),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Teriyaki Chicken',
+  //     'Chicken breast chunks with a teriyaki sauce served with rice and broccoli.',
+  //     'https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_center,w_730,h_913/k%2Farchive%2F01410577b65b0fa720ffaf8a514172d4e4c82e4f',
+  //     [
+  //       new Ingredient('Chicken Breast', 6, 'oz'),
+  //       new Ingredient('White Rice', 2, 'cup(s)'),
+  //       new Ingredient('Broccoli', 2, 'cup(s)'),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Cowboy Burger',
+  //     'Double cheeseburger with bacon, an onion ring, jalapenos, and BBQ sauce.',
+  //     'https://iamhomesteader.com/wp-content/uploads/2023/05/rodeo-burger-800x736.jpg',
+  //     [
+  //       new Ingredient('80/20 Ground Beef', 1, 'lb(s)'),
+  //       new Ingredient('Hamburger Buns', 6, 'each'),
+  //       new Ingredient('Pepperjack Cheese', 6, 'slices'),
+  //       new Ingredient('Bacon', 1, 'lb'),
+  //       new Ingredient('Onion Rings', 6, 'each'),
+  //       new Ingredient('Jalapeno', 2, 'each'),
+  //       new Ingredient('BBQ sauce of choice', 1, 'each'),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,10 +4,11 @@ import { Subject } from 'rxjs';
 export class ShoppingListService {
   ingredientChanged = new Subject<Ingredient[]>();
   startedEditing = new Subject<number>();
-  private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  // private ingredients: Ingredient[] = [
+  //   new Ingredient('Apples', 5),
+  //   new Ingredient('Tomatoes', 10),
+  // ];
+  private ingredients: Ingredient[] = [];
 
   getIngredients() {
     return this.ingredients.slice();
