a338ccd8c201ebf3bcd0d710dc0404b4528964ba
James Barber <jpbarber34@gmail.com>
2024-01-18 07:14:31 -0500

    Add some dynamic @if statements

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -124,6 +124,7 @@ export class AuthService {
   }
 
   private handleError(errorRes: HttpErrorResponse) {
+    console.log('error message test', errorRes);
     let errorMessage = 'An unknown error occurred!';
     if (!errorRes.error || !errorRes.error.error)
       return throwError(errorMessage);
@@ -139,6 +140,9 @@ export class AuthService {
         break;
       case 'INVALID_LOGIN_CREDENTIALS':
         errorMessage = 'Invalid login credentials';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER : Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.':
+        errorMessage = 'Too many failed login attempts, try again later';
         break;
     }
     return throwError(errorMessage);
