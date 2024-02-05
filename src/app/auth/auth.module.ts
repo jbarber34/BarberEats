cd70c34d0b8d6dd25d06e5e06bd729e8b8790320
James Barber <jpbarber34@gmail.com>
2024-02-05 16:14:47 -0500

    Implement lazy loading

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     FormsModule,
     SharedModule,
-    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    RouterModule.forChild([{ path: '', component: AuthComponent }]),
   ],
 })
 export class AuthModule {}
